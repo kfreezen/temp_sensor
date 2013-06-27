a671aa29a883bfc1a643f9fbde5f2142bbcf7544
Kent Friesen <kfreezen@gmail.com>
2013-06-27 15:45:41 -0700

    Misc changes

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ int main(int argc, char** argv) {
 
     sleep(1);
     
-    //XBAPI_Command(CMD_ATSM, command_data, 0xc0, TRUE);
+    XBAPI_Command(CMD_ATSM, command_data, 0xc0, TRUE);
     //XBAPI_Command(CMD_ATAC, 0, 0xc1, FALSE);
 
     // Initialize crc16 code.
@@ -98,7 +98,7 @@ int main(int argc, char** argv) {
         XBee_Wake();
         //sleep(1); // FIXME: This is an extremely bad way to do this.  Figure out a
         timer1_poll_delay_ms(14);
-        SendReport();
+        //SendReport();
         XBee_Sleep();
 
         sleep(59);
